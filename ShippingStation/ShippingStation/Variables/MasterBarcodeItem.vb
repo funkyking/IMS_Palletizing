455c1ec4886aa6bbfb0c4532c3a3416cb0eae13d
funkyking <paulanand894@gail.com>
2023-10-30 19:13:27 +0800

    Removed Everything From Database

--- ShippingStation/ShippingStation/Variables/MasterBarcodeItem.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class MasterBarcodeItem
-    Public Property WorkOrderID As Guid
-    Public Property PoNumber As String
-    Public Property WorkOrder As String
-    Public Property SubGroup As String
-    Public Property PalletNo As Integer
-    Public Property Shift As String
-End Class
